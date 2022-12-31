b3c2ac81365531a7308b232939c6f4b1b6f27f49
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2022-12-31 11:13:03 -0500

    Create GetNotionResourceModal.ts

---
+++ src/Modals/GetNotionResourceModal.ts
@@ -0,0 +1,39 @@
+import { App, Modal, Setting } from "obsidian";
+
+export class GetNotionResourceModal extends Modal {
+	result: string;
+	onSubmit: (result: string) => void;
+
+	constructor(app: App, onSubmit: (result: string) => void) {
+		super(app);
+		this.onSubmit = onSubmit;
+	}
+
+	onOpen() {
+		const { contentEl } = this;
+
+		contentEl.createEl("h1", { text: "Enter the URL of the page you want to sync." });
+
+		new Setting(contentEl)
+			.setName("URL")
+			.addText((text) =>
+				text.onChange((value) => {
+					this.result = value
+				}));
+
+		new Setting(contentEl)
+			.addButton((btn) =>
+				btn
+					.setButtonText("Submit")
+					.setCta()
+					.onClick(() => {
+						this.close();
+						this.onSubmit(this.result);
+					}));
+	}
+
+	onClose() {
+		let { contentEl } = this;
+		contentEl.empty();
+	}
+}
