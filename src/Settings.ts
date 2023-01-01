3795431480377cf8bef849737241ab6116635afe
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2023-01-01 08:28:20 -0500

    Update Settings.ts

--- src/Settings.ts
+++ src/Settings.ts
@@ -4,40 +4,39 @@ import {App, PluginSettingTab, Setting} from "obsidian";
 export class Settings extends PluginSettingTab {
 	plugin: Plugin;
 
+	homeFolder: string;
+	notionIntegrationToken: string;
+
 	constructor(app: App, plugin: Plugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
-	private exampleSetting() {
-		let {containerEl} = this;
-
-		return new Setting(containerEl)
-			.setName("Date format")
-			.setDesc("Default date format")
-			.addText((text) =>
-				text
-					.setPlaceholder("MMMM dd, yyyy")
-					.setValue(this.plugin.settings.dateFormat)
-					.onChange(async (value) => {
-						this.plugin.settings.dateFormat = value;
-						await this.plugin.saveSettings();
-					})
-			);
-	}
-
-	private homeFolderSetting() {
-		let {containerEl} = this;
-
+	private homeFolderSetting(containerEl: HTMLElement) {
 		return new Setting(containerEl)
 			.setName("Home Folder")
 			.setDesc("The name of the folder you want to use to host your Notion pages.")
 			.addText((text) =>
 				text
-					.setPlaceholder("MMMM dd, yyyy")
+					.setPlaceholder("Notion")
 					.setValue(this.plugin.settings.homeFolder)
 					.onChange(async (value) => {
 						this.plugin.settings.homeFolder = value;
+						await this.plugin.saveSettings();
+					})
+			);
+	}
+
+	private apiTokenSetting(containerEl: HTMLElement) {
+		return new Setting(containerEl)
+			.setName("Notion Integration Token")
+			.setDesc("The token used to connect to your workspace.")
+			.addText((text) =>
+				text
+					.setPlaceholder("---")
+					.setValue(this.plugin.settings.notionIntegrationToken)
+					.onChange(async (value) => {
+						this.plugin.settings.notionIntegrationToken = value;
 						await this.plugin.saveSettings();
 					})
 			);
@@ -48,6 +47,8 @@ export class Settings extends PluginSettingTab {
 
 		containerEl.empty();
 
-		this.homeFolderSetting();
+		this.homeFolderSetting(containerEl);
+
+		this.apiTokenSetting(containerEl);
 	}
 }
