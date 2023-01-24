eac5812df853cc9eadfadd429b81f6946bd34bcf
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2023-01-24 15:12:25 -0500

    Create NotionUrlSplitter.spec.ts

---
+++ tests/NotionUrlSplitter.spec.ts
@@ -0,0 +1,63 @@
+import {splitNotionUrl} from "../src/Misc/NotionUrlSplitter";
+import {assert} from "chai";
+
+
+describe('notion url splitter', () => {
+	describe('text w/o notion.so is given', function () {
+		it('should throw an exception', () => {
+			try {
+				splitNotionUrl("test")
+
+				assert.fail("Error was not thrown.")
+			} catch (e) {
+				it('should throw notion.so error', function () {
+					assert.equal(e.message,
+						"The url must be from 'notion.so'.");
+				});
+			}
+		})
+	});
+
+	describe('text has notion.so but is missing name or id', function () {
+		it('should throw an exception', () => {
+			try {
+				splitNotionUrl("https://www.notion.so/test")
+
+				assert.fail("Error was not thrown.")
+			} catch (e) {
+				it('should throw notion.so error', function () {
+					assert.equal(e.message,
+						"The url must be from 'notion.so'.");
+				});
+			}
+		})
+	});
+
+	describe('text has notion.so and name-id pair', () => {
+
+		it('should return object', function () {
+			const result = splitNotionUrl("https://www.notion.so/test-123")
+
+			assert.deepEqual(result,
+				{
+					name: "test",
+					id: "123",
+					url: "https://www.notion.so/test-123"
+				});
+		})
+	});
+
+	describe('text has notion.so and name-id pair with two dashes', () => {
+
+		it('should return object', function () {
+			const result = splitNotionUrl("https://www.notion.so/test-two-123")
+
+			assert.deepEqual(result,
+				{
+					name: "test-two",
+					id: "123",
+					url: "https://www.notion.so/test-two-123"
+				});
+		})
+	});
+})
