fdc563dee252cd85a515bc5375e4a8d6e46e6abe
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2023-01-24 15:12:21 -0500

    Update MarkdownTemplate.tsx

--- src/Views/MarkdownTemplate.tsx
+++ src/Views/MarkdownTemplate.tsx
@@ -1,6 +1,43 @@
 import * as React from "react";
-import {SplitNotionUrlInterface, PageApiBodyInterface} from "../Interfaces";
+import {SplitNotionUrlInterface, PageApiBodyInterface, CommonPropertyInterface} from "../Interfaces";
 import moment from "moment";
+import {
+	wrapInStrikethrough,
+	wrapInItalic,
+	wrapInCode,
+	wrapInUnderline,
+	wrapInLink,
+	wrapInBold
+} from "../Misc/MarkdownHelpers";
+
+const createPropertiesArray = (pageResponseBody: PageApiBodyInterface): Array<CommonPropertyInterface> => {
+
+	// @ts-ignore
+	return Object.values(pageResponseBody.properties);
+}
+
+const parseProperty =
+	(property: CommonPropertyInterface) => {
+		if (property.type == 'multi_select' && property.multi_select) {
+			return `${property.multi_select.map(x => x.name).join(', ')}`;
+		}
+	}
+
+const propertiesMetadata = (pageResponseBody: PageApiBodyInterface) => {
+	if (pageResponseBody.properties) {
+		return (
+			<li>
+				Properties
+				<ul>
+					{createPropertiesArray(pageResponseBody).map(property => (
+						<li>{parseProperty(property)}</li>
+					))
+					}
+				</ul>
+			</li>
+		);
+	}
+}
 
 const Metadata = (splitUrl: SplitNotionUrlInterface, pageResponseBody: PageApiBodyInterface) => {
 	return (
@@ -21,6 +58,7 @@ const Metadata = (splitUrl: SplitNotionUrlInterface, pageResponseBody: PageApiBo
 							At: {moment(pageResponseBody.last_edited_time).format('MMMM Do YYYY, h:mm:ss a')}</li>
 						<li>Type: {pageResponseBody.object}</li>
 						<li>Icon: {pageResponseBody.icon?.type == 'emoji' ? pageResponseBody.icon.emoji : 'None'}</li>
+						{propertiesMetadata(pageResponseBody)}
 					</ul>
 				</li>
 			</ul>
@@ -29,13 +67,50 @@ const Metadata = (splitUrl: SplitNotionUrlInterface, pageResponseBody: PageApiBo
 	);
 }
 
+const parseTitle = (pageResponseBody: PageApiBodyInterface) => {
+	let title = "";
+
+	if (pageResponseBody.properties.hasOwnProperty("Name")) {
+
+		pageResponseBody.properties.Name.title.forEach((fragment) => {
+			const text = fragment.text.content;
+
+			const link = fragment.text.link;
+
+			const annotations = fragment.annotations;
+
+			title += wrapInCode(
+				wrapInUnderline(
+					wrapInStrikethrough(
+						wrapInItalic(
+							wrapInBold(
+								wrapInLink(link, text), annotations.bold),
+							annotations.italic
+						), annotations.strikethrough), annotations.underline), annotations.code)
+		});
+
+		return title;
+
+	}
+}
+
+const getNotionCoverImage = (pageResponseBody: PageApiBodyInterface): string => {
+	return pageResponseBody.cover ? pageResponseBody.cover?.external.url : "";
+}
+
 export const MarkdownTemplate = (splitUrl: SplitNotionUrlInterface, pageResponseBody: PageApiBodyInterface) => {
 	return (
 		<div>
 			{Metadata(splitUrl, pageResponseBody)}
+			<p>%% startNotionTitle %%</p>
 			<h1>
-				{pageResponseBody.properties.title.title.map(x => x.text.content).join(' ')}
+				{parseTitle(pageResponseBody)}
 			</h1>
+			<p>%% endNotionTitle %%</p>
+
+			<p>%% startNotionCoverageImage %%</p>
+			<img src={getNotionCoverImage(pageResponseBody)} alt={`Cover image for ${splitUrl.name}`}/>
+			<p>%% endNotionCoverageImage %%</p>
 		</div>
 	);
 };
