2c5ed9f465147830d4aceeb33afe0c2bb869271e
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2023-02-10 18:51:44 -0500

    Update main.ts
    Found a way to solve the problem with .env variables not loading

--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
 // noinspection JSUnusedGlobalSymbols
 
+const basePath = (app.vault.adapter as any).basePath
+
+import * as dotenv from 'dotenv'
+dotenv.config({
+	path: `${basePath}/.obsidian/plugins/notion-x-obsidian-plugin/.env`,
+	debug: false
+})
+
 import Plugin from "./src/Plugin";
 export default Plugin;
