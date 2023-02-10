6e0b2f6949648de381ea4e0f35b5c11f74182298
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2023-02-10 18:53:53 -0500

    Update NotionApi.ts
    split callPageApi into three methods
    - callPostmanMockApi: calls the mock api handled by Postman
    - callProductionApi: calls the production proxy api
    - callPageApi: Handles the if/then logic based on environment

--- src/Misc/NotionApi.ts
+++ src/Misc/NotionApi.ts
@@ -1,15 +1,51 @@
 import {SplitNotionUrlInterface} from "../Interfaces";
 import axios from "axios";
+import {
+	mockProxyIsEnabled,
+	mockProxyApiToken,
+	mockProxyURL,
+	proxyURL,
+	mockProxyApiPageCallResponseStatusCode
+} from '../Config/Config';
 
 /**
- * Calls the api to get the page json response.
- * @param splitUrl : SplitNotionUrlInterface
- * @param rootUrl : string
- * @param token : string
+ * Calls the mock api built in Postman. For development purposes.
+ * @param splitUrl
+ * @param token
  */
-async function callPageApi(splitUrl: SplitNotionUrlInterface, rootUrl: string, token: string) {
+async function callPostmanMockApi(splitUrl: SplitNotionUrlInterface, token: string) {
+	const data = {
+		pageId: splitUrl.id,
+		notionIntegrationToken: token
+	}
 
-	const response = await axios.post(`${rootUrl}/api/pages/show`, {
+	axios.defaults.headers.common = {
+		"X-API-Key": mockProxyApiToken,
+	};
+
+	const config = {
+		method: 'post',
+		maxBodyLength: Infinity,
+		url: `${mockProxyURL}/api/pages/show?status=${mockProxyApiPageCallResponseStatusCode}`,
+		data: data
+	};
+
+	const response = await axios(config);
+
+	try {
+		return response;
+	} catch (e) {
+		throw new Error(e.message)
+	}
+}
+
+/**
+ * Calls the production api.
+ * @param splitUrl
+ * @param token
+ */
+async function callProductionApi(splitUrl: SplitNotionUrlInterface, token: string) {
+	const response = await axios.post(`${proxyURL}/api/pages/show`, {
 		pageId: splitUrl.id,
 		notionIntegrationToken: token
 	});
@@ -18,6 +54,20 @@ async function callPageApi(splitUrl: SplitNotionUrlInterface, rootUrl: string, t
 		return response;
 	} catch (e) {
 		throw new Error(e.message)
+	}
+}
+
+/**
+ * Calls the api to get the page json response.
+ * @param splitUrl : SplitNotionUrlInterface
+ * @param rootUrl : string
+ * @param token : string
+ */
+async function callPageApi(splitUrl: SplitNotionUrlInterface, token: string) {
+	if (mockProxyIsEnabled) {
+		return await callPostmanMockApi(splitUrl, token);
+	} else {
+		return await callProductionApi(splitUrl, token);
 	}
 }
 
