5aee74c982174234cc855231fcbc273396d6863e
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2023-02-11 16:08:24 -0500

    Update MarkdownHelpers.spec.ts

--- tests/MarkdownHelpers.spec.ts
+++ tests/MarkdownHelpers.spec.ts
@@ -10,108 +10,108 @@ import {
 
 describe("markdown helpers", () => {
 	describe("wrap in link", () => {
-		describe("https://google.com and 'Google' passed", () => {
-			it('should return <a href="https://google.com">Google</a>', () => {
-				const result = wrapInLink("https://google.com", "Google");
+		context("https://google.com and 'Google' passed", () => {
+			const result = wrapInLink("https://google.com", "Google");
 
+			it('should return <a href="https://google.com">Google</a>', () => {
 				assert.equal(result, `<a href="https://google.com">Google</a>`);
 			})
 		})
 
-		describe("no link and 'Google' passed", () => {
-			it('should return Google', () => {
-				const result = wrapInLink(null, "Google");
+		context("no link and 'Google' passed", () => {
+			const result = wrapInLink(null, "Google");
 
+			it('should return Google', () => {
 				assert.equal(result, `Google`);
 			})
 		})
 	})
 
 	describe("wrap in bold", () => {
-		describe("Google is passed with style = true", () => {
-			it('should return <strong>Google</strong>', () => {
-				const result = wrapInBold("Google", true);
+		context("Google is passed with style = true", () => {
+			const result = wrapInBold("Google", true);
 
+			it('should return <strong>Google</strong>', () => {
 				assert.equal(result, `<strong>Google</strong>`);
 			})
 		})
 
-		describe("Google is passed with style = false", () => {
-			it('should return Google', () => {
-				const result = wrapInBold("Google", false);
+		context("Google is passed with style = false", () => {
+			const result = wrapInBold("Google", false);
 
+			it('should return Google', () => {
 				assert.equal(result, `Google`);
 			})
 		})
 	})
 
 	describe("wrap in italics", () => {
-		describe("Google is passed with style = true", () => {
-			it('should return <em>Google</em>', () => {
-				const result = wrapInItalic("Google", true);
+		context("Google is passed with style = true", () => {
+			const result = wrapInItalic("Google", true);
 
+			it('should return <em>Google</em>', () => {
 				assert.equal(result, `<em>Google</em>`);
 			})
 		})
 
-		describe("Google is passed with style = false", () => {
-			it('should return Google', () => {
-				const result = wrapInItalic("Google", false);
+		context("Google is passed with style = false", () => {
+			const result = wrapInItalic("Google", false);
 
+			it('should return Google', () => {
 				assert.equal(result, `Google`);
 			})
 		})
 	})
 
 	describe("wrap in strikethrough", () => {
-		describe("Google is passed with style = true", () => {
-			it('should return <s>Google</s>', () => {
-				const result = wrapInStrikethrough("Google", true);
+		context("Google is passed with style = true", () => {
+			const result = wrapInStrikethrough("Google", true);
 
+			it('should return <s>Google</s>', () => {
 				assert.equal(result, `<s>Google</s>`);
 			})
 		})
 
-		describe("Google is passed with style = false", () => {
-			it('should return Google', () => {
-				const result = wrapInStrikethrough("Google", false);
+		context("Google is passed with style = false", () => {
+			const result = wrapInStrikethrough("Google", false);
 
+			it('should return Google', () => {
 				assert.equal(result, `Google`);
 			})
 		})
 	})
 
 	describe("wrap in underline", () => {
-		describe("Google is passed with style = true", () => {
-			it('should return <u>Google</u>', () => {
-				const result = wrapInUnderline("Google", true);
+		context("Google is passed with style = true", () => {
+			const result = wrapInUnderline("Google", true);
 
+			it('should return <u>Google</u>', () => {
 				assert.equal(result, `<u>Google</u>`);
 			})
 		})
 
-		describe("Google is passed with style = false", () => {
-			it('should return Google', () => {
-				const result = wrapInUnderline("Google", false);
+		context("Google is passed with style = false", () => {
+			const result = wrapInUnderline("Google", false);
 
+			it('should return Google', () => {
 				assert.equal(result, `Google`);
 			})
 		})
 	})
 
 	describe("wrap in code", () => {
-		describe("Google is passed with style = true", () => {
-			it('should return <code>Google</code>', () => {
-				const result = wrapInCode("Google", true);
+		context("Google is passed with style = true", () => {
+			const result = wrapInCode("Google", true);
 
+			it('should return <code>Google</code>', () => {
 				assert.equal(result, `<code>Google</code>`);
 			})
 		})
 
-		describe("Google is passed with style = false", () => {
-			it('should return Google', () => {
-				const result = wrapInCode("Google", false);
+		context("Google is passed with style = false", () => {
+			const result = wrapInCode("Google", false);
 
+			it('should return Google', () => {
 				assert.equal(result, `Google`);
 			})
 		})
