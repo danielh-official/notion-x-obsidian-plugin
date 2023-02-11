3f7fe1bfb302a3131465762f7949d8ee4dec512b
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2023-02-11 16:08:20 -0500

    Update Config.ts

--- src/Config/Config.ts
+++ src/Config/Config.ts
@@ -1,24 +1,34 @@
 /**
  * This is the server url the Postman mock uses.
  */
-export const mockProxyURL = process.env['MOCK_API_SERVER_URL'];
+export const mockProxyURL = () : string|null => {
+	return process.env['MOCK_API_SERVER_URL'] ?? null;
+};
 
 /**
  * This is the private server key required to call the Postman mock.
  */
-export const mockProxyApiToken = process.env['MOCK_API_SERVER_KEY'];
+export const mockProxyApiToken = () : string|null => {
+	return process.env['MOCK_API_SERVER_KEY'] ?? null;
+};
 
 /**
  * The mock api will only be called if this is enabled (i.e., true). It is disabled by default.
  */
-export const mockProxyIsEnabled = process.env['MOCK_PROXY_IS_ENABLED'] ?? false;
+export const mockProxyIsEnabled = () : boolean => {
+	return process.env['MOCK_PROXY_IS_ENABLED'] === 'true' ?? false
+};
 
 /**
  * Controls the status code of the response for calling the page api on the Postman mock.
  */
-export const mockProxyApiPageCallResponseStatusCode = process.env['MOCK_PROXY_API_PAGE_CALL_RETURN'] ?? 200;
+export const mockProxyApiPageCallResponseStatusCode = () : number => {
+	return process.env['MOCK_PROXY_API_PAGE_CALL_RETURN'] ? Number(process.env['MOCK_PROXY_API_PAGE_CALL_RETURN']) : 200;
+};
 
 /**
  * This is the production URL used to call the API.
  */
-export const proxyURL = 'https://example.com';
+export const proxyURL = () : string => {
+	return 'https://example.com';
+};
